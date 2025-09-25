591a0b3d60ed232be46973d27337677b4696ef40
AshworthETH <olcvkidwxf9879@hotmail.com>
2025-09-25 21:23:18 +0800

    recreate new front on netlify

--- src/wagmi.ts
+++
@@ -1,9 +0,0 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit'
-import { sepolia, hardhat } from 'wagmi/chains'
-
-export const config = getDefaultConfig({
-  appName: 'Number Gamble',
-  projectId: 'YOUR_WALLETCONNECT_PROJECT_ID', // Get this from https://cloud.walletconnect.com
-  chains: [sepolia, hardhat],
-  ssr: false,
-})
